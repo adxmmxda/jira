659851ae39d25382bf76793d59cdb50980c46378
adxmmxda <akramzodanodir005@gmail.com>
2024-05-17 10:19:26 +0500

    some changes

---
+++ myproject/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        spacing: {
+          '52': '13rem', 
+        },
+      },
+    },
+    variants: {
+      extend: {
+        space: ['responsive'],
+      },
+    },
+    plugins: [],
+  }
